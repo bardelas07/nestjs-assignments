6e431301af14ee2d32f8b48d5a91120f18bcb6dc
unknown <irvin.bardelas@cvsu.edu.ph>
2024-11-09 22:15:52 +0800

    Completed Assignment 2: Prime Numbers

---
+++ src/app.controller.ts
@@ -0,0 +1,28 @@
+import { BadRequestException, Controller, Get, Param } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+  @Get(':n')
+generateFibonacci(@Param('n') n: string): { sequence: number[] } {
+    const numberN = parseInt(n, 10);
+    if (isNaN(numberN) || numberN < 1) throw new BadRequestException('Please provide a valid positive integer for n.');
+    return { sequence: this.fibonacci(numberN) };
+}
+
+private fibonacci(n: number): number[] {
+    let [a, b] = [0, 1];
+    return Array.from({ length: n }, () => {
+        const current = a;
+        [a, b] = [b, a + b];
+        return current;
+    });
+}
+
+}
